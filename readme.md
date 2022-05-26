d281ccd9d5105f5235158ec79face924a66fa64c
Adelaide Baron <16630927@students.lincoln.ac.uk>
2022-05-26 23:56:19 +0100

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Fibonacci 🧮
+- **Authors:** Adelaide Baron, Will Tran, Moshain Khan 
+- **Purpose:** Obtain the nth term of the Fibonacci sequence by passing n to the method
+- **Purpose**: Requirement of week 1 Java with Sparta Global, practice using recursion
+- **Date:** 25/05/2022
+
+### Complete Group Task 
+*no more edits to be made*
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class App {
+    public static Integer FibbSeqNumber(Integer position) {
+        Integer FibbonacciTotal = 0;
+        //position == null
+        if (position == null) {
+            return 0;
+        } else if (position <= 0) {
+            FibbonacciTotal = 0;
+            //Base case
+        } else if (position <= 2) {
+            FibbonacciTotal = 1;
+        } else {
+            //Recursive
+            return FibbSeqNumber(position - 1) + FibbSeqNumber(position - 2);
+        }
+        return FibbonacciTotal;
+    }
+}
